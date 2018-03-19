fea89cc32b12d0b02259787f01923902fefa751e
tronfortytwo <emanuele.sorce@hotmail.com>
2018-03-19 13:39:52 +0100

    Removed otr scripts

--- scripts/isinstalled.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -d ~/.cache/ontheroad.emanuelesorce/bin ]; then
-	exit 0
-else
-	exit 1
-fi
--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r /.cache/ontheroad.emanuelesorce/bin/
-
-cp -v /opt/click.ubuntu.com/ontheroad.emanuelesorce/current/scripts/otr /home/phablet/.cache/ontheroad.emanuelesorce/bin/
-cp -v /opt/click.ubuntu.com/ontheroad.emanuelesorce/current/scripts/otr-adb /home/phablet/.cache/ontheroad.emanuelesorce/bin/
